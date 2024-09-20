8652c5993c119c9698222ef0bef2e15ce3e26148
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-20 03:02:11 +0300

    new version

--- JavaScript.js
+++ JavaScript.js
@@ -1,23 +1,4 @@
-var passwordHash = "1231819095";
 {
-    init1: function Login() {
-        var passwordInput = document.getElementById("login").value;
-        var password = "";
-        for (var i = 0; i < passwordInput.length; i++) {
-            password = Math.abs(~~((password << 5) - password + passwordInput.charCodeAt(i)));
-        }
-        localStorage.setItem("login", password);
-        if (localStorage.getItem("login") == passwordHash) {
-            loginDiv.style.display = "none";
-            mainDiv.style.display = "block";
-        } else {
-            loginDiv.style.display = "block";
-            mainDiv.style.display = "none";
-            document.getElementById("alert").style.display = "block";
-        }
-    }
-}
-window.onload = function () {
     var isWebView = false;
     var userAgent = navigator.userAgent;
     if (/Android/.test(userAgent)) {
@@ -29,13 +10,41 @@ window.onload = function () {
         }
     }
     if (isWebView) {
-        android.style.display = "none";
-		alert("app");
+        localStorage.setItem("AndroidAPP", "true");
     } else {
-        android.style.display = "Block";
-		alert("browser");
+        localStorage.setItem("AndroidAPP", "false");
     }
-};
+    if (localStorage.getItem("AndroidAPP") == "true" && document.getElementById("android")) {
+        android.style.display = "none";
+    }
+}
+{
+    if (localStorage.getItem("login") == "1231819095") {
+        loginDiv.style.display = "none";
+        mainDiv.style.display = "block";
+    } else {
+        loginDiv.style.display = "block";
+        mainDiv.style.display = "none";
+        document.getElementById("login").focus();
+    }
+}
+function Login() {
+    var passwordInput = document.getElementById("login").value;
+    var password = "";
+    for (var i = 0; i < passwordInput.length; i++) {
+        password = Math.abs(~~((password << 5) - password + passwordInput.charCodeAt(i)));
+    }
+    localStorage.setItem("login", password);
+    if (localStorage.getItem("login") == "1231819095") {
+        loginDiv.style.display = "none";
+        mainDiv.style.display = "block";
+    } else {
+        loginDiv.style.display = "block";
+        mainDiv.style.display = "none";
+        document.getElementById("alert").style.display = "block";
+        document.getElementById("login").focus();
+    }
+}
 function PressEnterToLogin() {
     if (event.which == 13 || event.keyCode == 13) {
         Login();
@@ -48,7 +57,25 @@ function Logout() {
 window.onscroll = function () {
     if (window.pageYOffset < 10) {
         document.getElementById("logout").style.top = "15px";
+        if (document.getElementById("android")) {
+            document.getElementById("android").style.top = "15px";
+        }
+        if (document.getElementById("notifications")) {
+            document.getElementById("notifications").style.top = "15px";
+        }
+        if (document.getElementById("about")) {
+            document.getElementById("about").style.top = "15px";
+        }
     } else {
         document.getElementById("logout").style.top = "-50px";
+        if (document.getElementById("android")) {
+            document.getElementById("android").style.top = "-50px";
+        }
+        if (document.getElementById("notifications")) {
+            document.getElementById("notifications").style.top = "-50px";
+        }
+        if (document.getElementById("about")) {
+            document.getElementById("about").style.top = "-50px";
+        }
     }
 };
